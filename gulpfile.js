30a47e86799b3b1d65d34b009fec1903c8046b22
daalar11 <52834541+daalar11@users.noreply.github.com>
2021-12-13 13:14:49 +0100

    d

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp=require('gulp');
 const sass=require('gulp-sass')(require('sass'));
-
+const {series} =require('gulp');
 function buildStlyes2(){
     return gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error',sass.logError))
         .pipe(gulp.dest('./css'))
         
 }
+function copy(){
+    return gulp.src('./node_modules/**/bootstrap.bundle.min.js')
+        .pipe(gulp.dest('./js'))
+        
+}
 exports.compila2=buildStlyes2
+exports.copy=copy
+exports.build=series(buildStlyes2,copy);
